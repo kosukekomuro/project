ba34a0311804bcb71c7feedaead9a33556e2ea2d
kosukekomuro <mmc.komuro@gmail.com>
2019-06-28 08:32:11 +0900

    HerokuのDBに接続

--- user/done_add_user.php
+++ user/done_add_user.php
@@ -16,20 +16,22 @@
       $user_pass = htmlspecialchars($user_pass, ENT_NOQUOTES, 'UTF-8');
 
       // データベースに接続
-      $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-      $user = 'root';
-      $passwoed = '';
+      $dsn = 'mysql:dbname=heroku_3eeeee4567fdbd1;host=us-cdbr-iron-east-02.cleardb.net;charset=utf8';
+      $user = 'b34326d2a64349';
+      $passwoed = 'a19be61d';
       $dbn = new PDO($dsn, $user, $passwoed);
       $dbn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       
       //SQLの発行
-      $sql = 'INSERT INTO user(name, password, created_at, updated_at) VALUES (?,?,?,?)';
+      $sql = 'INSERT INTO users(name, password, created_at, updated_at) VALUES (?,?,?,?)';
       $stmt = $dbn->prepare($sql);
 
       $data[] = $user_name;
       $data[] = $user_pass;
-      $data[] = null;
-      $data[] = null;
+      $data[] = date("Y/m/d H:i:s");
+      $data[] = date("Y/m/d H:i:s");
+      // $data[] = null;
+      // $data[] = null;
       print_r($data);
 
       $stmt->execute($data);
@@ -42,7 +44,7 @@
 
     }catch(Exception $e){
       // SQLのエラーは通常表示されないが、以下のようにすることで表示することができる
-      var_dump($pdo->errorInfo()); //PDOを使っているとき
+      // var_dump($pdo->errorInfo()); //PDOを使っているとき
       var_dump($stmt->errorInfo()); //PDOを使っているとき
       echo mysql_error(); //mysql関数を使っているとき
       
