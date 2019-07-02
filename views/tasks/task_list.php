66802885712edf39ddea692dc31b06b6bd53ff41
kosukekomuro <mmc.komuro@gmail.com>
2019-07-02 18:37:30 +0900

    タスクの登録

--- views/tasks/task_list.php
+++ views/tasks/task_list.php
@@ -9,8 +9,9 @@
   タスク一覧
   <form id="add_task_form" name="add_task_form" action="add_task.php" method="POST">
     <label for="task_name">タスク追加</label>
-    <input type="text" id="task_name" name="user_name" placeholder="タスク入力">
-</form>
+    <input type="text" id="task_name" name="task_name" placeholder="タスク入力">
+    <input type="submit" value="作成">
+  </form>
 </body>
 
 </html>
