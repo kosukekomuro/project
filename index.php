8e952de7b4a5cabb3b0fb313655c9cbf3648c3fd
kosukekomuro <mmc.komuro@gmail.com>
2019-07-04 09:57:25 +0900

    追加 ログイ画面css

--- index.php
+++ index.php
@@ -7,23 +7,27 @@
 </head>
 
 <body>
-  <header>
-    <h1 >Taskal へようこそ!</h1>
-  </header>
-  <main>
-    <form id="login_form" name="login_form" action="views/users/check_login.php" method="POST">
-      <label for="user_name">ユーザー名</label>
-      <input type="text" id="user_name" name="user_name" placeholder="ユーザーID">
-      <br>
-      <label for="user_password">パスワード</label>
-      <input type="passeord" id="user_password" name="user_password" placeholder="パスワード">
-      <br>
-      <input type="submit" id="login_button" name="login_button" value="ログイン">
-    </form>
-  </main>
-    <a href = "/project/views/users/add_user.php">
-      新規登録
-    </a>
+  <div class="container-feild main-background taskal-container">
+    <main class="login">
+      <header>
+        <h1 class="title">ログイン</h1>
+      </header>
+
+      <!-- <div class="row">
+        <div class="col-sm-2" style="background-color:red;">Red</div>
+        <div class="col" style="background-color:blue;">Blue</div>
+        <div class="col-sm-2" style="background-color:yellow;">Yellow</div>
+      </div> -->
+      <form id="login_form" class="login-form" name="login_form" action="views/users/check_login.php" method="POST">
+        <input type="text" id="user_name" class="login-form__name" name="user_name" placeholder="ユーザー名">
+        <input type="passeord" id="user_password" class="login-form__password" name="user_password" placeholder="パスワード">
+        <input type="submit" id="login_button" class="login-form__login-btn" name="login_button" value="ログイン">
+        <a class="login-form__signup-btn" href ="/project/views/users/add_user.php">
+          <span>新規登録</span>
+        </a>
+      </form>
+    </main>
+  </div>
 </body>
 
 </html>
