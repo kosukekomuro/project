67f16a443d2ea46c381643a465ab7a36d5df018d
kosukekomuro <mmc.komuro@gmail.com>
2019-07-07 12:27:07 +0900

    追加　タスクの順番（タスクの作成時）

--- views/tasks/add_task.php
+++ views/tasks/add_task.php
@@ -19,12 +19,38 @@
       // データベースへの接続
       $dbn = connection_to_db();
 
-      // todo taskの詳細をのちに追加
-      $sql='INSERT INTO tasks(task_name, user_id) VALUES(?, ?)';
+      //SQLの発行
+      $sql = 'SELECT task_name, id, task_order FROM tasks WHERE user_id = ? ORDER BY task_order DESC LIMIT 1';
       $stmt = $dbn->prepare($sql);
+      $data[] = $_SESSION['user_id'];
+      
+      // // stmtには検索したデータの一覧が代入うされている
+      $stmt->execute($data);
+
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      $data = array();
+      // print_r($sql);
+      // $dbn = null;
+      //データベースへの接続
+      // $dbn = connection_to_db();
+
+      // todo taskの詳細をのちに追加
+      $sql='INSERT INTO tasks(task_name, user_id, task_order) VALUES(?, ?, ?)';
+      $stmt = $dbn->prepare($sql);
+
+
 
       $data[] = $task_name;
       $data[] = $_SESSION['user_id'];
+
+      if($rec == true){
+        $data[] = $rec['task_order'] + 1;
+      }else{
+        $data[] = 0;
+      }
+        
+
+      print_r($data);
 
       // var_dump($data);
       // print '<br>';
@@ -36,9 +62,9 @@
       header('Location: task_list.php');
 
     }catch(Exception $e){
-      // var_dump($pdo->errorInfo()); //PDOを使っているとき
-      // var_dump($stmt->errorInfo()); //PDOを使っているとき
-      // echo mysql_error(); //mysql関数を使っているとき
+      var_dump($pdo->errorInfo()); //PDOを使っているとき
+      var_dump($stmt->errorInfo()); //PDOを使っているとき
+      echo mysql_error(); //mysql関数を使っているとき
       print 'ただいま障害により大変ご迷惑をおかけしております';
     }
   }
