ed36b28cc7c54d35086fc36e30dbb0475320e639
kosukekomuro <mmc.komuro@gmail.com>
2019-08-10 20:38:30 +0900

    エラーメッセージファイルの作成

--- assets/js/checkError.js
+++ assets/js/checkError.js
@@ -29,56 +29,56 @@ const checkInputConfirmPassword = (confirmPass, password) =>{
 
 window.onload = () => {
   // 入力チェックを行う。
-  $('.login-form').on('submit', (e) =>{
+  // $('.login-form').on('submit', (e) =>{
 
-    $(".error-message").remove();
+  //   $(".error-message").remove();
 
-    // ユーザー名が空欄の時
-    if(checkInput(document.login_form.user_name.value)){
-      let html = `<p class="error-message">
-                    ユーザー名が入力されていません
-                  </p>`
+  //   // ユーザー名が空欄の時
+  //   if(checkInput(document.login_form.user_name.value)){
+  //     let html = `<p class="error-message">
+  //                   ユーザー名が入力されていません
+  //                 </p>`
       
-      $('.login-form__name').after(html);
-      e.preventDefault();
-    };
+  //     $('.login-form__name').after(html);
+  //     e.preventDefault();
+  //   };
 
-    // パスワードが空欄の時
-    if(checkInput(document.login_form.user_password.value)){
-      let html = `<p class="error-message">
-                    パスワードが入力されていません
-                  </p>`
+  //   // パスワードが空欄の時
+  //   if(checkInput(document.login_form.user_password.value)){
+  //     let html = `<p class="error-message">
+  //                   パスワードが入力されていません
+  //                 </p>`
 
-      $('.login-form__password').after(html);
-      e.preventDefault();
-    };
+  //     $('.login-form__password').after(html);
+  //     e.preventDefault();
+  //   };
 
-    // Todo ログイン判定を非同期う
-    // if(loginCheckFlag){
-    //   data = {user_name: document.login_form.user_name.value,
-    //           user_pass: document.login_form.user_password.value
-    //         };
+  //   // Todo ログイン判定を非同期う
+  //   // if(loginCheckFlag){
+  //   //   data = {user_name: document.login_form.user_name.value,
+  //   //           user_pass: document.login_form.user_password.value
+  //   //         };
 
-    //   $.ajax({
-    //     url: "views/users/check_login.php",
-    //     type: "POST",
-    //     data: data,
-    //     dataType: 'json',
-    //   })
-    //   .done(function(data){
-    //     console.log("成功");
-    //     console.log(data);
-    //   })
-    //   .fail(function(XMLHttpRequest, textStatus, errorThrown, data){
-    //     alert('通信に失敗しました。再度読み込んでください');
-    //     console.log("ajax通信に失敗しました");
-    //     console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-    //     console.log("textStatus     : " + textStatus);
-    //     console.log("errorThrown    : " + errorThrown.message);
-    //     console.log(data);
-    //   });
-    // };
-  });
+  //   //   $.ajax({
+  //   //     url: "views/users/check_login.php",
+  //   //     type: "POST",
+  //   //     data: data,
+  //   //     dataType: 'json',
+  //   //   })
+  //   //   .done(function(data){
+  //   //     console.log("成功");
+  //   //     console.log(data);
+  //   //   })
+  //   //   .fail(function(XMLHttpRequest, textStatus, errorThrown, data){
+  //   //     alert('通信に失敗しました。再度読み込んでください');
+  //   //     console.log("ajax通信に失敗しました");
+  //   //     console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+  //   //     console.log("textStatus     : " + textStatus);
+  //   //     console.log("errorThrown    : " + errorThrown.message);
+  //   //     console.log(data);
+  //   //   });
+  //   // };
+  // });
 
   // 入力チェックを行う。
   $('.sign-up-form').on('submit', (e) =>{
--- assets/js/main.js
+++ assets/js/main.js
@@ -183,6 +183,57 @@ $(function(){
     document.update_task_form.reset();
     $(".error-message").remove();
   });
+
+  $('.login-form').on('submit', (e) =>{
+
+    $(".error-message").remove();
+
+    // ユーザー名が空欄の時
+    if(checkInput(document.login_form.user_name.value)){
+      let html = `<p class="error-message">
+                    ユーザー名が入力されていません
+                  </p>`
+      
+      $('.login-form__name').after(html);
+      e.preventDefault();
+    };
+
+    // パスワードが空欄の時
+    if(checkInput(document.login_form.user_password.value)){
+      let html = `<p class="error-message">
+                    パスワードが入力されていません
+                  </p>`
+
+      $('.login-form__password').after(html);
+      e.preventDefault();
+    };
+
+    // Todo ログイン判定を非同期う
+    // if(loginCheckFlag){
+    //   data = {user_name: document.login_form.user_name.value,
+    //           user_pass: document.login_form.user_password.value
+    //         };
+
+    //   $.ajax({
+    //     url: "views/users/check_login.php",
+    //     type: "POST",
+    //     data: data,
+    //     dataType: 'json',
+    //   })
+    //   .done(function(data){
+    //     console.log("成功");
+    //     console.log(data);
+    //   })
+    //   .fail(function(XMLHttpRequest, textStatus, errorThrown, data){
+    //     alert('通信に失敗しました。再度読み込んでください');
+    //     console.log("ajax通信に失敗しました");
+    //     console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+    //     console.log("textStatus     : " + textStatus);
+    //     console.log("errorThrown    : " + errorThrown.message);
+    //     console.log(data);
+    //   });
+    // };
+  });
 });
 
 
