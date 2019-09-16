c0897932a3acfd2065a32a185844512bd818ee08
kosukekomuro <mmc.komuro@gmail.com>
2019-09-16 16:48:16 +0900

    ユーザー情報更新機能の更新

--- models/users/users.php
+++ models/users/users.php
@@ -54,7 +54,6 @@
   /**
    * ユーザーの重複登録チェック
    * @param string name
-   * @param string password
    * @return array
    *            int code 結果コード
    *            string error_message
@@ -97,6 +96,54 @@
       'count' => $count,
       'error_message' => "",
       'code' => 1
+    ];
+
+    return $return_data;
+  };
+
+  /**
+   * ユーザー情報の獲得
+   * @param string id
+   * @return array
+   *            int code 結果コード
+   *            string error_message
+   *
+   * @author komuro
+   **/
+  function user_select($id){
+
+    try{
+      // データベースに接続
+      $dbn = connection_to_db();
+
+      // SQLの準備
+      $sql = 'SELECT name FROM users WHERE id = ?';
+      $stmt = $dbn->prepare($sql);
+      $data[] = $id;
+
+      // stmtには検索したデータの一覧が代入されている
+      $stmt->execute($data);
+
+      // データベースから切断する。
+      $dbn = null;
+
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    }catch(Exception $e){
+
+      // その他のエラー時の戻り値
+      $return_data = [
+        'error_message' => $e,
+        'code' => 4
+      ];
+
+      return $return_data;
+    }
+
+    // 正常時の戻り値
+    // sqlが正しく稼働した場合、正常反応を返す
+    $return_data = [
+      'name' => $rec['name']
     ];
 
     return $return_data;
---
+++ views/users/update_user.php
@@ -0,0 +1,35 @@
+<?php
+  // 共通の関数読み込み;
+  require_once('../../common/common.php');
+
+  //セッションの確認 
+  check_session();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include ('../common/heads/head2.html'); ?>
+</head>
+
+<body>
+  <div class="container-feild main-background taskal-container">
+    <header class="index-header">
+        <img src="../../assets/image/main-logo.png">
+    </header>
+    <main class="sign-up">
+      <h1 class="sign-ip__title">ユーザー登録</h1>
+      <form method = "post" action = "../../models/users/create_user.php" class="sign-up-form" name="sign_up_form">
+        <input type="text" name="user_name" placeholder="ユーザー名" class="sign-up-form__name">
+        <input type="password" name="user_password" placeholder="パスワード" class="sign-up-form__password">
+        <input type="password" name="user_confirm_password" placeholder="再確認パスワード" class="sign-up-form__confirm-password">
+        <div class = "sign-up-form__btn">
+          <input type="submit" value="登録" class="sign-up-form__btn--blue">
+          <button type="button" onclick="history.back()" class="sign-up-form__btn--blue">戻る</button>
+      </div>
+      </form>
+    </main>
+  </div>  
+</body>
+
+</html>
